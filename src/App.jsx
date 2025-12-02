990dd2f229488aeff836873a69bc693225144763
Dipak Pahari <dipsonpahari@gmail.com>
2025-12-02 16:27:40 +0545

    Added navigation bar with scss converted every file to scss to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
+import React from 'react'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Project from './pages/Project.jsx'
+import Contact from './pages/Contact.jsx'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-white ">
-      <Navbar />
+    <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Project" element={<Project />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.scss';
+import logo from '../assets/logo.png';
+
+
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 text-white flex justify-between items-center ">
-      <Link to="/">
-        <img src="/vite.svg" alt="Logo" className="h-8 w-8 inline-block mr-2" />
-      </Link>
+    <nav className="Navbar">
+      <div className="Inner-Navbar">
+        <Link to="/">
+          <img src={logo} alt="Portfolio Logo" className="" />
+        </Link>
 
-      <div>
-        <Link to="/" className="text-white hover:text-gray-300 mx-2">
-        HOME
-        </Link>
-        <Link to="/about" className="text-white hover:text-gray-300 mx-2">
-        ABOUT
-        </Link>
-        <Link to="/project" className="text-white hover:text-gray-300 mx-2">
-        PROJECT
-        </Link>
-        <Link to="/contact" className="text-white hover:text-gray-300 mx-2">
-        CONTACT
-        </Link>
+        <div className="nav-links">
+          <Link to="/" className="text-white hover:text-gray-300 mx-2">
+          HOME
+          </Link>
+          <Link to="/About" className="text-white hover:text-gray-300 mx-2">
+          ABOUT
+          </Link>
+          <Link to="/Project" className="text-white hover:text-gray-300 mx-2">
+          PROJECT
+          </Link>
+          <Link to="/Contact" className="text-white hover:text-gray-300 mx-2">
+          CONTACT
+          </Link>
+        </div>
       </div>
     </nav>
   );
