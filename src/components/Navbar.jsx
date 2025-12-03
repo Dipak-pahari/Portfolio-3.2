b5827e6fe4033c4be2da0fa0bd995d70375a8179
Dipak Pahari <dipsonpahari@gmail.com>
2025-12-03 17:57:34 +0545

    Completed basic ui of Navbar and navigaitons with repsonsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,54 @@
-import React from 'react';
-import { Link, useLocation} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.scss';
 import logo from '../assets/logo.svg';
 import hamburger from '../assets/hamburger.png';
-import cross from "../assets/cross.png"
+import cross from "../assets/cross.png";
 
 //hamburger component
-function Hamburger() {
-  return(
-    <div>
-    <div className="hamburger-container">
-          <img src={hamburger} alt="hamburger icon" />
-    </div>
+function Hamburger({ openMenu }) {
+  return (
+    <div className="toggle-button-menu">
+      <div className="hamburger-container" style={{ display: openMenu ? "none" : "flex" }}>
+        <img src={hamburger} alt="hamburger icon" />
+      </div>
 
-    <div className="cross-container">
-    <img src={cross} alt="cross icon" />
-    </div>
+      <div className="cross-container" style={{ display: openMenu ? "flex" : "none" }}>
+        <img src={cross} alt="cross icon" />
+      </div>
     </div>
   );
 }
 
-
 // Navigation text component
-function Navtext({text, to}) {
-  const location = useLocation();        // get current URL
-  const isActive = location.pathname === to; // check if link matches
+function Navtext({ text, to }) {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
   return (
     <div className="Navtext-container">
-      <Link to={to} className={`Navigation-Link ${isActive ? 'active' : ''}`}
-      >
-          {text}
-          </Link>
-      <div className="Navtext-underline">
-      </div>
+      <Link to={to} className={`Navigation-Link ${isActive ? 'active' : ''}`}>
+        {text}
+      </Link>
+      <div className="Navtext-underline"></div>
     </div>
   );
 }
 
 // Navbar component
 function Navbar() {
+  const [openMenu, setMenuState] = useState(false);
+
   return (
     <nav className="Navbar">
       <div className="Inner-Navbar">
         <Link to="/">
-          <img src={logo} alt="Portfolio Logo" className="" />
+          <img src={logo} alt="Portfolio Logo" />
         </Link>
 
-        <Hamburger />
+        <div className="Toggle-Menu" onClick={() => setMenuState(!openMenu)}>
+          <Hamburger openMenu={openMenu} />
+        </div>
 
         <div className="nav-links">
           <Navtext text="HOME" to="/" />
@@ -55,8 +57,16 @@ function Navbar() {
           <Navtext text="CONTACT" to="/contact" />
         </div>
       </div>
+
+      {/* MOBILE MENU */}
+      <div className={`mobile-menu ${openMenu ? "show" : ""}`}>
+        <Navtext text="HOME" to="/" />
+        <Navtext text="ABOUT" to="/about" />
+        <Navtext text="PROJECT" to="/project" />
+        <Navtext text="CONTACT" to="/contact" />
+      </div>
+
     </nav>
-    
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import bgimg from '../assets/bg-img.png';
 
 const Home = () => {
   return (
-    <div>
+    <div className="Hero-banner-container">
+      <img src={bgimg} alt="Dipak's Photo" />
       <h1>Home Page</h1>
     </div>
   );
